6e6b4b780b91954f1cc65b347e06663d18d9e695
leijurv <leijurv@gmail.com>
2016-11-28 08:52:44 -0800

    better map creation

--- src/compiler/lex/TokenMapping.java
+++ src/compiler/lex/TokenMapping.java
@@ -8,7 +8,9 @@ import compiler.Operator;
 import compiler.token.Token;
 import compiler.token.TokenType;
 import static compiler.token.TokenType.*;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.Map;
 import javax.xml.ws.WebServiceException;
 
 /**
@@ -16,24 +18,38 @@ import javax.xml.ws.WebServiceException;
  * @author leijurv
  */
 public class TokenMapping {
-    private static final HashMap<String, Token> MAPPINGS = new HashMap<>();
+    private static final Map<String, Token> MAPPINGS;
     static {
-        MAPPINGS.put("≠", OPERATOR.create(Operator.NOT_EQUAL));
-        MAPPINGS.put("≥", OPERATOR.create(Operator.GREATER_OR_EQUAL));
-        MAPPINGS.put("≤", OPERATOR.create(Operator.LESS_OR_EQUAL));
-        put(SETEQUAL.create(false));
-        put(SETEQUAL.create(true));
+        HashMap<String, Token> map = new HashMap<>();
+        map.put("≠", OPERATOR.create(Operator.NOT_EQUAL));
+        map.put("≥", OPERATOR.create(Operator.GREATER_OR_EQUAL));
+        map.put("≤", OPERATOR.create(Operator.LESS_OR_EQUAL));
+        put(SETEQUAL.create(false), map);
+        put(SETEQUAL.create(true), map);
         for (Operator op : Operator.values()) {
-            put(OPERATOR.create(op));
+            put(OPERATOR.create(op), map);
         }
         for (TokenType tt : TokenType.values()) {
             if (tt.primitive()) {
-                put(tt);
+                put(tt, map);
+            }
+        }
+        MAPPINGS = Collections.unmodifiableMap(map);
+        verifySane();
+    }
+    public static void verifySane() {
+        for (String s : MAPPINGS.keySet()) {
+            switch (s.length()) {
+                case 1:
+                case 2:
+                    continue;
+                default:
+                    throw new IllegalStateException("Length " + s.length() + " illegal for mapping " + s);
             }
         }
     }
-    public static void put(Token t) {
-        MAPPINGS.put(t.toString(), t);
+    public static void put(Token t, Map<String, Token> map) {
+        map.put(t.toString(), t);
     }
     public static boolean mapsToToken(String s) {
         return MAPPINGS.containsKey(s);

fc195c97106c31037ffd0940d3550567d53f27f9
leijurv <leijurv@gmail.com>
2016-11-29 11:43:40 -0800

    lld format

--- src/compiler/x86/X86Format.java
+++ src/compiler/x86/X86Format.java
@@ -15,17 +15,13 @@ import java.util.stream.Collectors;
  */
 public class X86Format {
     public static final boolean MAC = System.getProperty("os.name").toLowerCase().contains("mac");
+    private static final String LLD_FORMAT = "lldformatstring:\n"
+            + "	.asciz	\"%lld\\n\"\n";
     private static final String HEADER_MAC = "    .section    __TEXT,__text,regular,pure_instructions\n"
             + "    .macosx_version_min 10, 10\n";
-    private static final String FOOTER_MAC = "\n"
-            + ".section	__TEXT,__cstring,cstring_literals\n"
-            + "lldformatstring:\n"
-            + "	.asciz	\"%lld\\n\"\n"
-            + ".subsections_via_symbols\n";
+    private static final String FOOTER_MAC = "\n.section	__TEXT,__cstring,cstring_literals\n" + LLD_FORMAT;
     private static final String HEADER_LINUX = ".text\n";
-    private static final String FOOTER_LINUX = "\n.section .rodata\n"
-            + "lldformatstring:\n"
-            + "	.asciz	\"%lld\\n\"\n";
+    private static final String FOOTER_LINUX = "\n.section .rodata\n" + LLD_FORMAT;
     private static final String HEADER = MAC ? HEADER_MAC : HEADER_LINUX;
     private static final String FOOTER = MAC ? FOOTER_MAC : FOOTER_LINUX;
     public static String assembleFinalFile(List<Pair<String, List<TACStatement>>> functions) {

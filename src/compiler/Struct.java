43f516c295484e5511b031fbe6539ceed1d9a479
leijurv <leijurv@gmail.com>
2016-12-08 11:07:28 -0800

    better

--- src/compiler/Struct.java
+++ src/compiler/Struct.java
@@ -10,7 +10,6 @@ import compiler.token.Token;
 import compiler.token.TokenType;
 import compiler.type.Type;
 import compiler.util.Parse;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
@@ -34,25 +33,20 @@ public class Struct {
         if (!fields.isEmpty()) {
             return;
         }
-        ArrayList<String> fieldNames = new ArrayList<>(lines.size());
-        ArrayList<Type> fieldTypes = new ArrayList<>(lines.size());
+        int pos = 0;
         for (int j = 0; j < lines.size(); j++) {
             Line thisLine = lines.get(j);
             List<Token> tokens = thisLine.getTokens();
             if (tokens.get(tokens.size() - 1).tokenType() != TokenType.VARIABLE) {
                 throw new RuntimeException();
             }
-            fieldNames.add((String) tokens.get(tokens.size() - 1).data());
-            Type tft = Parse.typeFromTokens(tokens.subList(0, tokens.size() - 1), context);
-            if (tft == null) {
+            String fieldName = (String) tokens.get(tokens.size() - 1).data();
+            Type fieldType = Parse.typeFromTokens(tokens.subList(0, tokens.size() - 1), context);
+            if (fieldType == null) {
                 throw new IllegalStateException("Unable to determine type of " + tokens.subList(0, tokens.size() - 1));
             }
-            fieldTypes.add(tft);
-        }
-        int pos = 0;
-        for (int i = 0; i < fieldTypes.size(); i++) {
-            fields.put(fieldNames.get(i), context.new VarInfo(fieldNames.get(i), fieldTypes.get(i), pos, true));
-            pos += fieldTypes.get(i).getSizeBytes();
+            fields.put(fieldName, context.new VarInfo(fieldName, fieldType, pos, true));
+            pos += fieldType.getSizeBytes();
         }
     }
     public VarInfo getFieldByName(String name) {

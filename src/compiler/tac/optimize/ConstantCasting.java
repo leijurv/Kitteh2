5bbe7ea3df561d8c9e3df7c96dd97dafea587ae7
leijurv <leijurv@gmail.com>
2016-12-13 09:38:47 -0800

    actually do the constant casting optimization

--- src/compiler/tac/optimize/ConstantCasting.java
+++ src/compiler/tac/optimize/ConstantCasting.java
@@ -7,6 +7,9 @@ package compiler.tac.optimize;
 import compiler.tac.TACCast;
 import compiler.tac.TACConst;
 import compiler.tac.TACStatement;
+import compiler.type.TypeFloat;
+import compiler.type.TypeNumerical;
+import compiler.x86.X86Const;
 import java.util.List;
 
 /**
@@ -19,13 +22,7 @@ public class ConstantCasting extends TACOptimization {
         for (int i = 0; i < block.size() - 1; i++) {
             if (block.get(i) instanceof TACConst) {
                 TACConst con = (TACConst) block.get(i);
-                if (con.params[0] == null) {
-                    try {
-                        Integer.parseInt(con.paramNames[0]);
-                    } catch (NumberFormatException e) {
-                        continue;
-                    }
-                } else {
+                if (!(con.params[0] instanceof X86Const)) {
                     continue;
                 }
                 if (!UselessTempVars.isTempVariable(con.paramNames[1])) {
@@ -33,8 +30,15 @@ public class ConstantCasting extends TACOptimization {
                 }
                 if (block.get(i + 1) instanceof TACCast) {
                     TACCast cast = (TACCast) block.get(i + 1);
-                    if (cast.paramNames[0].equals(con.paramNames[1])) {
-                        con.replace(con.paramNames[1], cast.paramNames[1], cast.params[1]);
+                    if (cast.paramNames[0].equals(con.paramNames[1])) {//cast source must equal const destination
+                        TypeNumerical castingTo = (TypeNumerical) cast.params[1].getType();//cast destination type
+                        if (castingTo instanceof TypeFloat) {
+                            continue;//lol its not like you can do: movss $5, %xmm1
+                        }
+                        String constantBeingCasted = con.paramNames[0];
+                        X86Const casted = new X86Const(constantBeingCasted, castingTo);
+                        con.replace(con.paramNames[0], constantBeingCasted, casted);//replace source with: the constant, now with the correct casted type
+                        con.replace(con.paramNames[1], cast.paramNames[1], cast.params[1]);//replace dest with: the destination of the original cast
                         block.remove(i + 1);
                         i--;
                     }

a94faeaf7da685a23cc7eb776d60d8eb73e85ece
leijurv <leijurv@gmail.com>
2016-12-13 08:58:21 -0800

    also remove after unconditional as well as return

--- src/compiler/tac/optimize/AfterReturn.java
+++ src/compiler/tac/optimize/AfterReturn.java
@@ -4,11 +4,14 @@
  * and open the template in the editor.
  */
 package compiler.tac.optimize;
+import compiler.tac.TACJump;
 import compiler.tac.TACReturn;
 import compiler.tac.TACStatement;
 import java.util.List;
 
 /**
+ * Any statement after a return / unconditional jump and before a jump
+ * destination is unreachable.
  *
  * @author leijurv
  */
@@ -16,7 +19,7 @@ public class AfterReturn extends TACOptimization {
     @Override
     protected void run(List<TACStatement> block, int blockBegin) {
         for (int i = 0; i < block.size(); i++) {
-            if (block.get(i) instanceof TACReturn) {
+            if (block.get(i) instanceof TACReturn || block.get(i).getClass() == TACJump.class) {
                 while (i + 1 < block.size()) {
                     block.remove(i + 1);
                 }

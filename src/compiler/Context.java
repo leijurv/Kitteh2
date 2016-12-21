75ca1bece0ff6259d29c9ed1f1acb40da0d11a5d
leijurv <leijurv@gmail.com>
2016-12-21 10:03:35 -0800

    now you can compile files containing the letter k

--- src/compiler/Context.java
+++ src/compiler/Context.java
@@ -97,7 +97,7 @@ public class Context {
         this.varIndex = null;
         this.imports = new HashMap<>();
         this.packageName = packageName;
-        String wewlad = packageName.substring(packageName.lastIndexOf('/') + 1).split(".k")[0];
+        String wewlad = packageName.substring(packageName.lastIndexOf('/') + 1).split("\\.k")[0];
         imports.put(wewlad, wewlad);
     }
     public HashMap<String, TypeStruct> structsCopy() {

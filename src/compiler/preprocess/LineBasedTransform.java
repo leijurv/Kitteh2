fb35714ee7aa61ae0fec5cd3c9bdba47b92e813e
leijurv <leijurv@gmail.com>
2017-01-31 12:59:42 -0800

    use bifunction

--- src/compiler/preprocess/LineBasedTransform.java
+++ src/compiler/preprocess/LineBasedTransform.java
@@ -5,11 +5,10 @@
  */
 package compiler.preprocess;
 import compiler.parse.Transform;
-import compiler.util.Pair;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.function.Function;
+import java.util.function.BiFunction;
 import java.util.function.Predicate;
 import java.util.stream.IntStream;
 
@@ -18,17 +17,17 @@ import java.util.stream.IntStream;
  * @author leijurv
  */
 public abstract class LineBasedTransform implements Transform<List<Line>> {
-    private static final Function<Pair<List<?>, Optional<Predicate<Integer>>>, IntStream> PARALLEL_INTSTREAM_FACTORY = i -> IntStream.range(0, i.getA().size()).parallel().filter(ind -> i.getB().isPresent() && i.getB().get().test(ind) || !i.getB().isPresent());
+    private static final BiFunction<List<?>, Optional<Predicate<Integer>>, IntStream> PARALLEL_INTSTREAM_FACTORY = (list, pred) -> IntStream.range(0, list.size()).parallel().filter(ind -> pred.isPresent() && pred.get().test(ind) || !pred.isPresent());
     protected abstract Line transform(Line line);
     @Override
     public final void apply(List<Line> lines) {
-        PARALLEL_INTSTREAM_FACTORY.apply(new Pair<>(lines, Optional.empty())).forEach(i -> {
+        PARALLEL_INTSTREAM_FACTORY.apply(lines, Optional.empty()).forEach(i -> {
             Line processed = runLine(lines.get(i));
             lines.set(i, processed);
         });
     }
     public final void apply(ArrayList<Object> maybeLines) {
-        PARALLEL_INTSTREAM_FACTORY.apply(new Pair<>(maybeLines, Optional.of(i -> maybeLines.get(i) instanceof Line))).forEach(i -> {
+        PARALLEL_INTSTREAM_FACTORY.apply(maybeLines, Optional.of(i -> maybeLines.get(i) instanceof Line)).forEach(i -> {
             Line processed = runLine((Line) maybeLines.get(i));
             maybeLines.set(i, processed);
         });

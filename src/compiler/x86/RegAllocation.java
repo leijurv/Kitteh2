0ef0eb2117e9bc4cfc5ee1176ea9a28d667b327d
leijurv <leijurv@gmail.com>
2017-02-15 15:22:27 -0800

    different data

--- src/compiler/x86/RegAllocation.java
+++ src/compiler/x86/RegAllocation.java
@@ -276,9 +276,10 @@ public class RegAllocation {
         }
         return false;
     }
-    public static void allocate(List<X86Function> fns) {
+    public static void allocate(List<X86Function> fns) {//TODO check usage of data structures here. are list and hashset the best choices?
+        HashSet<X86Function> toGo = new HashSet<>(fns);
         while (true) {
-            List<X86Function> ta = fns.stream().filter(X86Function::canAllocate).collect(Collectors.toList());
+            List<X86Function> ta = toGo.stream().filter(X86Function::canAllocate).collect(Collectors.toList());
             if (compiler.Compiler.verbose()) {
                 System.out.println("Allocating " + ta);
             }
@@ -286,6 +287,7 @@ public class RegAllocation {
                 return;
             }
             ta.parallelStream().forEach(X86Function::allocate);
+            toGo.removeAll(ta);
         }
     }
 }
